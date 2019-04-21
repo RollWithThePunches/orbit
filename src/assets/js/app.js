a2d8679bb6e4a16764573b12c785320ede280793
RollWithThePunches <remuccari@gmail.com>
2019-04-21 02:20:38 -0400

    Add cone Add cone visibility eventListener Remove circle Rotate moon

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,43 +1,61 @@
+import { ColorKeywords } from "three";
 
 ;(() => {
 
-   const orbitData = [
+    const orbitData = [
         {rotateZ: -0.01, lightY: -2, cameraZ: 0.5},
-        {rotateZ: 0.01, lightY: 0, cameraZ: -0.5}
-    ];
+		{rotateZ: 0.01, lightY: 0, cameraZ: -0.5},
+		{rotateZ: 0.05, lightY: 7, cameraZ: -0.7},
+		{rotateZ: -0.05, lightY: 0, cameraZ: 0.7},
+	];
 
+	const coneX = -14; 
+	const coneY = 16; 
+	const coneZ = 40;
+
+	const windowHeight = window.innerHeight;
+	let trigger;
+	let triggerCones;
+	
+	// Scene
 	const globeContainer = document.getElementById('globe-container');
 	const scene = new THREE.Scene();
-	const color1 = new THREE.DirectionalLight(0xFFFFFF);
+	const color = new THREE.DirectionalLight(0xFFFFFF);
 
+	// Camera
 	const camera = new THREE.PerspectiveCamera(
 		55, window.innerWidth / window.innerHeight, 0.1, 1000
 	);
 
+	// Render
 	const renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	globeContainer.appendChild(renderer.domElement);
 
+	// Sphere
  	const geometry = new THREE.SphereGeometry(40, 50, 50);
-    THREE.ImageUtils.crossOrigin = true;
-     
-    const circle = new THREE.CircleGeometry(1, 50, 50);
- 	const meshLambertCircle = new THREE.MeshLambertMaterial({ color: 0xdddddd });
- 	const meshCircle = new THREE.Mesh(circle, meshLambertCircle);
-	scene.add(meshCircle);
+	THREE.ImageUtils.crossOrigin = true;
+	geometry.rotateZ(-0.3);
+	geometry.rotateY(0.25);
+	 
+	// Cone shape
+	const cone = new THREE.CylinderGeometry(1, 0, 10, 50, false);
+	const meshLambertCone = new THREE.MeshLambertMaterial({ color: 0x13a31a });
+	const meshCone = new THREE.Mesh(cone, meshLambertCone);
+	scene.add(meshCone);
 
-	meshCircle.position.x = -11;
-	meshCircle.position.y = 10;
-	meshCircle.position.z = 50;
-
+	meshCone.position.x = coneX;
+	meshCone.geometry.rotateX(Math.PI * 0.5);
+	meshCone.position.y = coneY;
+	meshCone.position.z = coneZ;
+	meshCone.visible = false;
 
  	// Camera controls
  	const controls = new THREE.OrbitControls(camera);
-
     // No vertical rotating
     controls.minPolarAngle = Math.PI / 2;
-    controls.maxPolarAngle = Math.PI / 2;
-
+	controls.maxPolarAngle = Math.PI / 2;
+	
     // Rotation has minimum starting in dark side
     // and maximum from front Moon angle
     controls.minAzimuthAngle = - Math.PI;
@@ -56,8 +74,8 @@
 
 		// Make texture repeat
 		// May not actually need this
-		texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-		texture.repeat.set(1, 1);
+		// texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+		// texture.repeat.set(1, 1);
 
 	 	const material = new THREE.MeshLambertMaterial({
 	 		map: texture
@@ -73,28 +91,27 @@
 	camera.position.set(-0.41, 0, -120);
 	camera.lookAt(scene.position.set);
 
-	const light = color1;
+	// Light
+	const light = color;
  	light.position.set(10, 0, 25);
  	scene.add(light);
 
 	// Change autoRotateSpeed to negative for reverse
-    controls.autoRotateSpeed = -6;
-
-    // Event handler for animations in scroll
+	controls.autoRotateSpeed = -6;
+	
+    // Event handler for moon animations on scroll
 	const animateCamera = () => {
-		let elems, windowHeight;
 		const init = () => {
-		    elems = document.getElementsByClassName('trigger');
-		    windowHeight = window.innerHeight;
-		    _addEventHandlers();
+		    trigger = document.getElementsByClassName('trigger');
+		    addEventHandlers();
 		}
-	    const _addEventHandlers = () =>  {
-	    	window.addEventListener('scroll', _checkPosition);
+	    const addEventHandlers = () =>  {
+	    	window.addEventListener('scroll', checkPosition);
 	    	// window.addEventListener('resize', init)
 	  	}
-	  	const _checkPosition = () => {
-	    	for (let i = 0; i < elems.length; i++) {
-	      		let posFromTop = elems[i].getBoundingClientRect().top
+	  	const checkPosition = () => {
+	    	for (let i = 0; i < trigger.length; i++) {
+	      		let posFromTop = trigger[i].getBoundingClientRect().top
 		      	if (posFromTop - windowHeight <= 0) {
 
 					geometry.rotateZ(orbitData[i].rotateZ);
@@ -104,20 +121,45 @@
 		      	}
 	    	}
   		}
-			return {
-			    init: init
-			}
+		return {
+			init: init
+		}
 	}
 	animateCamera().init();
 
+	// Event handler for showing cones on scroll
+	const animateCones = () => {
+		const initCones = () => {
+		    triggerCones = document.getElementsByClassName('trigger-cone');
+		    addEventHandlersCones();
+		}
+	    const addEventHandlersCones = () =>  {
+	    	window.addEventListener('scroll', checkPositionCones);
+	  	}
+	  	const checkPositionCones = () => {
+	    	for (let i = 0; i < triggerCones.length; i++) {
+	      		let posFromTop = triggerCones[i].getBoundingClientRect().top
+		      	if (posFromTop - windowHeight <= 0) {
+
+					meshCone.traverse((elem) => {
+						if (elem instanceof THREE.Mesh) {
+							elem.visible = true;
+						}
+					});
+					
+		      	}
+	    	}
+  		}
+		return {
+			initCones: initCones
+		}
+	}
+	animateCones().initCones();
 
  	// Render spinning animation function
  	const render = () => {
  		requestAnimationFrame(render);
-
-
  		controls.update();
-
  		renderer.render(scene, camera);
  	}
 
